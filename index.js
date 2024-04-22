39aa019f13a47cae8f06e41961c476813da6fa47
giorgichakhvashvili <163650953+giorgichakhvashvili@users.noreply.github.com>
2024-04-22 20:05:37 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// const faqClick = document.querySelector(".faq-click");
+// const faqHidden = document.querySelector(".faq-hidden")
+
+// faqClick.addEventListener("click", () => {
+//     if(faqHidden.style.display ==='none') {
+//         faqHidden.style.display ='block';
+//     } else {
+//         faqHidden.style.display ='none';
+//     }
+// });
